4ffb45ee999281b3cad68a58baa0b413c53ae954
mikser <miklyaev_sv@mail.ru>
2016-10-09 01:22:40 +0800

    First commit

---
+++ src/main/java/myejb/project/com/Main.java
@@ -0,0 +1,11 @@
+package myejb.project.com;
+
+/**
+ * Created by mikser on 08.10.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
